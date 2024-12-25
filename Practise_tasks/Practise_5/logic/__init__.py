dac2280c2a9adcb9198857c141341a8af3c92e40
Daryana <b.darya2809@gmail.com>
2024-12-25 21:49:33 +0300

    Delete Practise_tasks/Practise_5 directory

--- Practise_tasks/Practise_5/logic/__init__.py
+++
@@ -1,5 +0,0 @@
-from logic.makeMove import makeMove
-from logic.chooseWord import chooseWord
-from logic.showWord import showWord
-from logic.setTries import setTries
-from logic.Record import record_update
