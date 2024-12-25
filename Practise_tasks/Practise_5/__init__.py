6561c8fbf488ea7c6dc5332fc315e9275c3fafcc
Daryana <b.darya2809@gmail.com>
2024-12-25 21:41:34 +0300

    Update and rename Practise_tasks/Practise_5/__init__.py to Practise_tasks/Practise_5/logic/__init__.py

--- Practise_tasks/Practise_5/__init__.py
+++
@@ -1,5 +0,0 @@
-from gameLogic.makeMove import makeMove
-from gameLogic.chooseWord import chooseWord
-from gameLogic.showWord import showWord
-from gameLogic.setTries import setTries
-from gameLogic.Record import record_update
---
+++ Practise_tasks/Practise_5/logic/__init__.py
@@ -0,0 +1,5 @@
+from logic.makeMove import makeMove
+from logic.chooseWord import chooseWord
+from logic.showWord import showWord
+from logic.setTries import setTries
+from logic.Record import record_update
